2e929cc820c869ccb2c5fa454bb5e938c1283c01
aulinda buban <buban.139456@cubao.sti.edu.ph>
2020-10-19 23:13:49 +0800

    Me

---
+++ simcalcu/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simcalcu
+{
+    public delegate int GetAnswer(int num1, int num2);
+    class Class1
+    {
+        public delegate int GetAnswer(int num1, int num2);
+        public static int getAdd(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public static int getDiff(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        public static int getMul(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        public static int getDiv(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
+    
+
---
+++ simcalcu/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace simcalcu
+{
+    public partial class Form1 : Form
+    {
+        int num1, num2;
+        GetAnswer delegatedAdd, delegatedDif, delegatedMul, delegatedDiv;
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            delegatedDiv = new GetAnswer(Class1.getDiv);
+            num1 = Int32.Parse(textBox1.Text);
+            num2 = Int32.Parse(textBox2.Text);
+            MessageBox.Show(delegatedDiv(num1, num2).ToString(), "Answer");
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+                delegatedDif = new GetAnswer(Class1.getDiff);
+                num1 = Int32.Parse(textBox1.Text);
+                num2 = Int32.Parse(textBox2.Text);
+                MessageBox.Show(delegatedDif(num1, num2).ToString(), "Answer");
+            }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            {
+                delegatedMul = new GetAnswer(Class1.getMul);
+                num1 = Int32.Parse(textBox1.Text);
+                num2 = Int32.Parse(textBox2.Text);
+                MessageBox.Show(delegatedMul(num1, num2).ToString(), "Answer");
+            }
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            delegatedAdd = new GetAnswer(Class1.getAdd);
+            num1 = Int32.Parse(textBox1.Text);
+            num2 = Int32.Parse(textBox2.Text);
+            MessageBox.Show(delegatedAdd(num1, num2).ToString(), "Answer");
+        }
+
+
+    }
+}
+
